010eea8d6fc9919507e6fc4380731e5e9355e645
Jathavaan Shankarr <jathavaan12@gmail.com>
2026-01-29 14:07:53 +0100

    #58 Added dynamic CTEs to handle paths that does not exist

--- src/application/contracts/conflation_service_interface.py
+++ src/application/contracts/conflation_service_interface.py
@@ -9,22 +9,30 @@ from src.domain.enums import Theme
 
 class IConflationService(ABC):
     @abstractmethod
-    def get_fkb_osm_id_relations(self, release: str, theme: Theme) -> pd.DataFrame:
+    def get_fkb_osm_id_relations(self, release: str, theme: Theme, region: str) -> pd.DataFrame:
         """
         Creates a DataFrame with all IDs from FKB and OSM for a given release and theme, and relates them to each other.
         :param release: Release identifier 'yyyy-mm-dd.x'
         :param theme: Theme enum
+        :param region: Region ID
         :return:
         """
         raise NotImplementedError
 
     @abstractmethod
-    def merge_fkb_osm(self, release: str, theme: Theme, ids: pd.DataFrame) -> Dict[str, list[gpd.GeoDataFrame]]:
+    def merge_fkb_osm(
+            self,
+            release: str,
+            region: str,
+            theme: Theme,
+            ids: pd.DataFrame
+    ) -> list[gpd.GeoDataFrame]:
         """
         Selects geometries based on priority and merges them.
         :param release: Release identifier 'yyyy-mm-dd.x'
+        :param region: Region ID
         :param theme: Theme enum
         :param ids: ID dataframe that relates FKB and OSM IDs
-        :return: A dictionary with region as key and another dictionary as value. The outer dictionary is keyed by region, and the inner is keyed by the spatial hash
+        :return: A list of GeoDataFrames. Each element is a partition that is partitioned by the geohash
         """
         raise NotImplementedError
--- src/infra/infrastructure/services/conflation_service.py
+++ src/infra/infrastructure/services/conflation_service.py
@@ -1,32 +1,37 @@
-﻿from typing import Dict
-
+﻿import geopandas as gpd
 import pandas as pd
-import geopandas as gpd
 from duckdb import DuckDBPyConnection
 from shapely import from_wkb
 
 from src.application.common import logger
-from src.application.contracts import IConflationService, IFilePathService
+from src.application.contracts import IConflationService, IFilePathService, IBlobStorageService
 from src.domain.enums import Theme, StorageContainer, DataSource, EPSGCode
 
 
 class ConflationService(IConflationService):
     __db_context: DuckDBPyConnection
     __file_path_service: IFilePathService
+    __blob_storage_service: IBlobStorageService
 
-    def __init__(self, db_context: DuckDBPyConnection, file_path_service: IFilePathService):
+    def __init__(
+            self,
+            db_context: DuckDBPyConnection,
+            file_path_service: IFilePathService,
+            blob_storage_service: IBlobStorageService
+    ):
         self.__db_context = db_context
         self.__file_path_service = file_path_service
+        self.__blob_storage_service = blob_storage_service
 
-    def get_fkb_osm_id_relations(self, release: str, theme: Theme) -> pd.DataFrame:
-        logger.info(f"Finding diff between the raw OSM- and FKB-datasets.")
+    def get_fkb_osm_id_relations(self, release: str, theme: Theme, region: str) -> pd.DataFrame:
+        logger.info(f"Finding diff between the raw OSM- and FKB-datasets for region '{region}'.")
 
         osm_release = self.__file_path_service.create_virtual_filesystem_path(
             storage_scheme="az",
             container=StorageContainer.RAW,
             release=release,
             theme=Theme.BUILDINGS,
-            region="*",
+            region=region,
             file_name="*.parquet",
             dataset=DataSource.OSM.value
         )
@@ -36,32 +41,52 @@ class ConflationService(IConflationService):
             container=StorageContainer.RAW,
             release=release,
             theme=Theme.BUILDINGS,
-            region="*",
+            region=region,
             file_name="*.parquet",
             dataset=DataSource.FKB.value
         )
 
-        ids_df = self.__db_context.execute(
-            f'''
-            WITH fkb AS (
-                SELECT
-                    external_id AS fkb_id,
-                    TRY_CAST(building_id AS INTEGER) AS building_id,
-                    CAST(FLOOR(ST_X(ST_Centroid(geometry)) * 100) AS INTEGER) AS grid_x,
-                    CAST(FLOOR(ST_Y(ST_Centroid(geometry)) * 100) AS INTEGER) AS grid_y,
-                    ST_Force2D(geometry) AS geom,
-                FROM '{fkb_release}'
-            ),
+        osm_path_base = self.__file_path_service.remove_blob_file_name_from_path(
+            file_path=osm_release,
+            file_name="*.parquet",
+            prefix="az://raw/"
+        )
 
-            osm AS (
-                SELECT
-                    external_id AS osm_id,
-                    TRY_CAST(building_id AS INTEGER) AS building_id,
-                    CAST(FLOOR(ST_X(ST_Centroid(geometry)) * 100) AS INTEGER) AS grid_x,
-                    CAST(FLOOR(ST_Y(ST_Centroid(geometry)) * 100) AS INTEGER) AS grid_y,
-                    ST_Force2D(geometry) AS geom,
-                FROM '{osm_release}'
-            ),
+        fkb_path_base = self.__file_path_service.remove_blob_file_name_from_path(
+            file_path=fkb_release,
+            file_name="*.parquet",
+            prefix="az://raw/"
+        )
+
+        has_osm_files = self.__blob_storage_service.has_files_under_blob_path_base(
+            container=StorageContainer.RAW,
+            path=osm_path_base
+        )
+
+        has_fkb_files = self.__blob_storage_service.has_files_under_blob_path_base(
+            container=StorageContainer.RAW,
+            path=fkb_path_base
+        )
+
+        osm_count = self.__db_context.execute(
+            f"SELECT COUNT(*) AS count FROM '{osm_release}'"
+        ).fetchone()[0] if has_osm_files else 0
+
+        fkb_count = self.__db_context.execute(
+            f"SELECT COUNT(*) AS count FROM '{fkb_release}'"
+        ).fetchone()[0] if has_fkb_files else 0
+
+        logger.info(f"Total number of buildings: {osm_count} in OSM and {fkb_count} FKB")
+
+        osm_cte, fkb_cte = ConflationService.__create_cte(
+            has_osm_files=has_osm_files,
+            has_fkb_files=has_fkb_files,
+            osm_release=osm_release,
+            fkb_release=fkb_release
+        )
+
+        query = f'''
+            WITH {fkb_cte}, {osm_cte},
 
             candidate_fkb_only_buildings AS (
                 SELECT
@@ -126,12 +151,12 @@ class ConflationService(IConflationService):
                     fkb_id,
                     osm_id,
                 FROM fkb_only
-                UNION
+                UNION ALL
                 SELECT
                     fkb_id,
                     osm_id,
                 FROM osm_only
-                UNION
+                UNION ALL
                 SELECT
                     fkb_id,
                     osm_id,
@@ -141,26 +166,33 @@ class ConflationService(IConflationService):
 
             SELECT * FROM merged
             '''
-        ).fetchdf()
+
+        ids_df = self.__db_context.execute(query).fetchdf()
 
         osm_only_count = ids_df[ids_df["fkb_id"].isna()].shape[0]
         fkb_only_count = ids_df[ids_df["osm_id"].isna()].shape[0]
         overlapping_count = ids_df[ids_df["fkb_id"].notna() & ids_df["osm_id"].notna()].shape[0]
 
         logger.info(
-            f"Conflation step completed. Result: {osm_only_count} OSM-only buildings, {fkb_only_count} FKB-only buildings, and {overlapping_count} overlapping buildings."
+            f"Conflation step for region '{region}' completed. Result: {osm_only_count} OSM-only buildings, {fkb_only_count} FKB-only buildings, and {overlapping_count} overlapping buildings."
         )
 
         return ids_df
 
-    def merge_fkb_osm(self, release: str, theme: Theme, ids: pd.DataFrame) -> Dict[str, list[gpd.GeoDataFrame]]:
-        logger.info(f"Merging OSM- and FKB-datasets")
+    def merge_fkb_osm(
+            self,
+            release: str,
+            region: str,
+            theme: Theme,
+            ids: pd.DataFrame
+    ) -> list[gpd.GeoDataFrame]:
+        logger.info(f"Merging OSM- and FKB-datasets for region '{region}'")
         osm_release = self.__file_path_service.create_virtual_filesystem_path(
             storage_scheme="az",
             container=StorageContainer.RAW,
             release=release,
             theme=Theme.BUILDINGS,
-            region="*",
+            region=region,
             file_name="*.parquet",
             dataset=DataSource.OSM.value
         )
@@ -170,7 +202,7 @@ class ConflationService(IConflationService):
             container=StorageContainer.RAW,
             release=release,
             theme=Theme.BUILDINGS,
-            region="*",
+            region=region,
             file_name="*.parquet",
             dataset=DataSource.FKB.value
         )
@@ -184,24 +216,129 @@ class ConflationService(IConflationService):
         fkb_filter = "FALSE" if not ids_fkb else f"external_id IN ({ids_fkb_str})"
         osm_filter = "FALSE" if not ids_osm else f"external_id IN ({ids_osm_str})"
 
-        df = self.__db_context.execute(
-            f'''
-            WITH fkb AS (
-                SELECT 
-                    external_id,
-                    ST_AsWKB(geometry) AS geometry,
-                    bbox,
-                    region,
-                    partition_key,
-                    TRY_CAST(building_type AS VARCHAR) AS building_type, 
+        osm_path_base = self.__file_path_service.remove_blob_file_name_from_path(
+            file_path=osm_release,
+            file_name="*.parquet",
+            prefix="az://raw/"
+        )
+
+        fkb_path_base = self.__file_path_service.remove_blob_file_name_from_path(
+            file_path=fkb_release,
+            file_name="*.parquet",
+            prefix="az://raw/"
+        )
+
+        has_osm_files = self.__blob_storage_service.has_files_under_blob_path_base(
+            container=StorageContainer.RAW,
+            path=osm_path_base
+        )
+
+        has_fkb_files = self.__blob_storage_service.has_files_under_blob_path_base(
+            container=StorageContainer.RAW,
+            path=fkb_path_base
+        )
+
+        osm_cte, fkb_cte = ConflationService.__create_cte_2(
+            has_osm_files=has_osm_files,
+            osm_release=osm_release,
+            osm_filter=osm_filter,
+            has_fkb_files=has_fkb_files,
+            fkb_release=fkb_release,
+            fkb_filter=fkb_filter
+        )
+
+        query = f'''
+                WITH {fkb_cte}, {osm_cte},
+                
+                conflated AS (
+                    SELECT * FROM fkb
+                    UNION ALL
+                    SELECT * FROM osm
+                )
+                
+                SELECT * FROM conflated
+                '''
+
+        df = self.__db_context.execute(query).fetchdf()
+
+        df["geometry"] = df["geometry"].apply(lambda g: bytes(g) if isinstance(g, (bytearray, memoryview)) else g)
+        df["geometry"] = df["geometry"].apply(from_wkb)
+        gdf = gpd.GeoDataFrame(df, geometry="geometry", crs=f"EPSG:{EPSGCode.WGS84.value}")
+
+        merged_count = gdf.shape[0]
+        logger.info(f"Merged dataset for region '{region}' contains {merged_count} entries")
+
+        return [
+            gpd.GeoDataFrame(hash_partition, geometry="geometry", crs=gdf.crs)
+            for _, hash_partition in gdf.groupby("partition_key")
+        ]
+
+    @staticmethod
+    def __create_cte(has_osm_files: bool, has_fkb_files: bool, osm_release: str, fkb_release: str) -> tuple[str, str]:
+        if has_osm_files:
+            osm_cte = f"""osm AS 
+            (
+                SELECT
+                    external_id AS osm_id,
                     TRY_CAST(building_id AS INTEGER) AS building_id,
-                    NULL AS feature_update_time,
-                    NULL AS feature_capture_time,
-                    'fkb' AS source
-                FROM '{fkb_release}'
-                WHERE {fkb_filter}
-            ),
-            osm AS (
+                    CAST(FLOOR(ST_X(ST_Centroid(geometry)) * 100) AS INTEGER) AS grid_x,
+                    CAST(FLOOR(ST_Y(ST_Centroid(geometry)) * 100) AS INTEGER) AS grid_y,
+                    ST_Force2D(geometry) AS geom
+                FROM read_parquet('{osm_release}', union_by_name = true)
+            )
+            """
+        else:
+            osm_cte = """osm AS 
+            (
+                SELECT
+                    CAST(NULL AS INTEGER) AS osm_id,
+                    CAST(NULL AS INTEGER) AS building_id,
+                    CAST(NULL AS INTEGER) AS grid_x,
+                    CAST(NULL AS INTEGER) AS grid_y,
+                    CAST(NULL AS GEOMETRY) AS geom
+                WHERE FALSE
+            )
+            """
+
+        if has_fkb_files:
+            fkb_cte = f"""fkb AS 
+            (
+                SELECT
+                    external_id AS fkb_id,
+                    TRY_CAST(building_id AS INTEGER) AS building_id,
+                    CAST(FLOOR(ST_X(ST_Centroid(geometry)) * 100) AS INTEGER) AS grid_x,
+                    CAST(FLOOR(ST_Y(ST_Centroid(geometry)) * 100) AS INTEGER) AS grid_y,
+                    ST_Force2D(geometry) AS geom
+                FROM read_parquet('{fkb_release}', union_by_name = true)
+            )
+            """
+        else:
+            fkb_cte = """fkb AS 
+            (
+                SELECT
+                    CAST(NULL AS VARCHAR) AS fkb_id,
+                    CAST(NULL AS INTEGER) AS building_id,
+                    CAST(NULL AS INTEGER) AS grid_x,
+                    CAST(NULL AS INTEGER) AS grid_y,
+                    CAST(NULL AS GEOMETRY) AS geom
+                WHERE FALSE
+            )
+            """
+
+        return osm_cte, fkb_cte
+
+    @staticmethod
+    def __create_cte_2(
+            has_osm_files: bool,
+            has_fkb_files: bool,
+            osm_release: str,
+            fkb_release: str,
+            osm_filter,
+            fkb_filter
+    ) -> tuple[str, str]:
+        if has_osm_files:
+            osm_cte = f"""osm AS 
+            (
                 SELECT 
                     external_id,
                     ST_AsWKB(geometry) as geometry,
@@ -213,27 +350,62 @@ class ConflationService(IConflationService):
                     feature_update_time,
                     feature_capture_time,
                     'osm' AS source
-                FROM '{osm_release}'
+                FROM read_parquet('{osm_release}', union_by_name = true)
                 WHERE {osm_filter}
-            ),
-            
-            conflated AS (
-                SELECT * FROM fkb
-                UNION
-                SELECT * FROM osm
             )
-            
-            SELECT * FROM conflated
-            '''
-        ).fetchdf()
+            """
+        else:
+            osm_cte = """
+            osm AS (
+                SELECT
+                    CAST(NULL AS INTEGER) AS external_id,
+                    CAST(NULL AS BLOB) AS geometry,
+                    CAST(NULL AS STRUCT(minx DOUBLE, miny DOUBLE, maxx DOUBLE, maxy DOUBLE)) AS bbox,
+                    CAST(NULL AS VARCHAR) AS region,
+                    CAST(NULL AS VARCHAR) AS partition_key,
+                    CAST(NULL AS VARCHAR) AS building_type,
+                    CAST(NULL AS INTEGER) AS building_id,
+                    CAST(NULL AS TIMESTAMP) AS feature_update_time,
+                    CAST(NULL AS TIMESTAMP) AS feature_capture_time,
+                    'osm' AS source
+                WHERE FALSE
+            )
+            """
 
-        df["geometry"] = df["geometry"].apply(lambda g: bytes(g) if isinstance(g, (bytearray, memoryview)) else g)
-        df["geometry"] = df["geometry"].apply(from_wkb)
-        gdf = gpd.GeoDataFrame(df, geometry="geometry", crs=f"EPSG:{EPSGCode.WGS84.value}")
+        if has_fkb_files:
+            fkb_cte = f"""fkb AS 
+            (
+                SELECT 
+                    external_id,
+                    ST_AsWKB(geometry) AS geometry,
+                    bbox,
+                    region,
+                    partition_key,
+                    TRY_CAST(building_type AS VARCHAR) AS building_type,
+                    TRY_CAST(building_id AS INTEGER) AS building_id,
+                    NULL AS feature_update_time,
+                    NULL AS feature_capture_time,
+                    'fkb' AS source
+                FROM read_parquet('{fkb_release}', union_by_name = true)
+                WHERE {fkb_filter}
+            )
+            """
+        else:
+            fkb_cte = """
+            fkb AS (
+                SELECT
+                    CAST(NULL AS VARCHAR) AS external_id,
+                    CAST(NULL AS BLOB) AS geometry,
+                    CAST(NULL AS STRUCT(minx DOUBLE, miny DOUBLE, maxx DOUBLE, maxy DOUBLE)) AS bbox,
+                    CAST(NULL AS VARCHAR) AS region,
+                    CAST(NULL AS VARCHAR) AS partition_key,
+                    CAST(NULL AS VARCHAR) AS building_type,
+                    CAST(NULL AS INTEGER) AS building_id,
+                    CAST(NULL AS TIMESTAMP) AS feature_update_time,
+                    CAST(NULL AS TIMESTAMP) AS feature_capture_time,
+                    'fkb' AS source
+                WHERE FALSE
+            )
+            """
 
-        return {
-            str(region): [
-                gpd.GeoDataFrame(hash_partition, geometry="geometry", crs=gdf.crs)
-                for _, hash_partition in region_partition.groupby("partition_key")
-            ] for region, region_partition in gdf.groupby("region")
-        }
+        return osm_cte, fkb_cte
