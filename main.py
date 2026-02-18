baa3c1c92fa44d27b88601d0e66cb23b0171634d
Jathavaan Shankarr <jathavaan12@gmail.com>
2026-02-18 12:32:51 +0100

    #67 Added a CLI arguments to select which script to benchmark and run

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-﻿import uuid
+﻿import argparse
+import uuid
+from argparse import ArgumentError
 
 from src.application.common import monitor_cpu_and_ram
 from src.presentation.configuration import initialize_dependencies
-from src.presentation.entrypoints import run_pipeline
+from src.presentation.entrypoints import run_pipeline, blob_storage_db_scan
 
 RUN_ID = str(uuid.uuid4())
 
 
-@monitor_cpu_and_ram(run_id=RUN_ID, query_id="osm-fkb-conflation")
+@monitor_cpu_and_ram(run_id=RUN_ID, query_id="main")
 def main() -> None:
     initialize_dependencies()
-    run_pipeline()
+    script_id = get_script_id()
+
+    match script_id:
+        case "conflation-pipeline":
+            run_pipeline()
+            return
+        case "blob-storage-db-scan":
+            blob_storage_db_scan()
+            return
+        case _:
+            raise ArgumentError(argument=None, message="Script ID was invalid")
+
+
+def get_script_id() -> str:
+    parser = argparse.ArgumentParser("doppa-data")
+    parser.add_argument("id", help="ID of script to run")
+    args = parser.parse_args()
+    return args.id
 
 
 if __name__ == "__main__":
