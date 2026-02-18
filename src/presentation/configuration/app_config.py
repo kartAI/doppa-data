e46b70a296dad4922f749e6bf6693e175eba4d71
Jathavaan Shankarr <jathavaan12@gmail.com>
2026-02-18 12:31:38 +0100

    #67 Added blob storage DB scan query

--- src/presentation/configuration/app_config.py
+++ src/presentation/configuration/app_config.py
@@ -5,3 +5,4 @@ def initialize_dependencies() -> None:
     container = Containers()
     container.wire(modules=["src.application.common.monitor"])
     container.wire(modules=["src.presentation.entrypoints.release_pipeline"])
+    container.wire(modules=["src.presentation.entrypoints.blob_storage_db_scan"])
---
+++ src/presentation/entrypoints/blob_storage_db_scan.py
@@ -0,0 +1,24 @@
+﻿from dependency_injector.wiring import Provide, inject
+from duckdb import DuckDBPyConnection
+
+from src.application.contracts import IFilePathService
+from src.domain.enums import StorageContainer, Theme
+from src.infra.infrastructure import Containers
+
+
+@inject
+def blob_storage_db_scan(
+        db_context: DuckDBPyConnection = Provide[Containers.db_context],
+        path_service: IFilePathService = Provide[Containers.file_path_service]
+) -> None:
+    path = ""
+    path_service.create_virtual_filesystem_path(
+        storage_scheme="az",
+        release="2026-02-16.3",
+        container=StorageContainer.DATA,
+        theme=Theme.BUILDINGS,
+        region="*",
+        file_name="*.parquet"
+    )
+
+    db_context.execute(f"SELECT count(*) AS count FROM read_parquet('{path}')")
