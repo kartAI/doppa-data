c24d5622c17d0157e5fe9a6d65738c9fc1a2c921
Jathavaan Shankarr <jathavaan12@gmail.com>
2026-02-25 14:14:53 +0100

    #87 Created query PostGIS DB scan query

---
+++ src/presentation/entrypoints/db_scan_postgis.py
@@ -0,0 +1,13 @@
+﻿from dependency_injector.wiring import inject, Provide
+from psycopg2.extensions import cursor
+
+from src.application.common.monitor import monitor_cpu_and_ram
+from src.infra.infrastructure import Containers
+
+
+@inject
+@monitor_cpu_and_ram(query_id="db-scan-postgis")
+def db_scan_postgis(
+        db_context: cursor = Provide[Containers.postgres_context]
+) -> None:
+    db_context.execute("SELECT count(*) AS count FROM 'buildings'")
