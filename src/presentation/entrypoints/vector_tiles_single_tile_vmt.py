96393274a770dd4b1fdc0b3f7d44b8a9e29538d9
Jathavaan Shankarr <jathavaan12@gmail.com>
2026-03-07 08:31:04 +0100

    #136 Fixed incorrect script ID

--- src/presentation/entrypoints/vector_tiles_single_tile_vmt.py
+++ src/presentation/entrypoints/vector_tiles_single_tile_vmt.py
@@ -6,7 +6,7 @@ from src.application.common.monitor_network import monitor_network
 Z, X, Y = 13, 4340, 2382
 
 
-@monitor_network(query_id="vector-tiles-single-tile-pmtiles")
+@monitor_network(query_id="vector-tiles-single-tile-vmt")
 def vector_tiles_single_tile_vmt() -> None:
     tile = requests.get(f"{Config.AZURE_VMT_SERVER_URL}/tiles/{Z}/{X}/{Y}")
     if tile is None:
