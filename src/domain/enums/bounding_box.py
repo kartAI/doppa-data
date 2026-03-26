5f00c97436a6f52efa635f362838c5f75cd66817
Jathavaan Shankarr <jathavaan12@gmail.com>
2026-03-26 14:59:44 +0100

    #172 Added benchmarks for different bbox sizes for local, blob storage and postgis

--- src/domain/enums/bounding_box.py
+++ src/domain/enums/bounding_box.py
@@ -2,4 +2,7 @@
 
 
 class BoundingBox(Enum):
+    NEIGHBORHOOD_WGS84 = (10.38, 63.42, 10.41, 63.44)
+    TRONDHEIM_WGS84 = (10.10, 63.27, 10.73, 63.52)
+    TRONDELAG_WGS84 = (9.00, 62.30, 15.50, 65.20)
     NORWAY_WGS84 = (4.0, 57.9, 31.3, 71.2)
---
+++ src/presentation/entrypoints/bbox_filtering_result_set_sizes_municipality_duckdb.py
@@ -0,0 +1,46 @@
+from dependency_injector.wiring import Provide, inject
+from duckdb import DuckDBPyConnection
+
+from src import Config
+from src.application.common.monitor_network import monitor_network
+from src.application.contracts import IFilePathService
+from src.application.dtos import CostConfiguration
+from src.domain.enums import StorageContainer, Theme, BenchmarkIteration, BoundingBox
+from src.infra.infrastructure import Containers
+
+
+def bbox_filtering_result_set_sizes_municipality_duckdb() -> None:
+    _benchmark()
+
+
+@inject
+@monitor_network(
+    query_id="bbox-filtering-result-set-sizes-municipality-duckdb",
+    benchmark_iteration=BenchmarkIteration.BBOX_FILTERING_RESULT_SET_SIZES,
+    cost_configuration=CostConfiguration(include_aci=True, include_blob_storage=True)
+)
+def _benchmark(
+        db_context: DuckDBPyConnection = Provide[Containers.duckdb_context],
+        path_service: IFilePathService = Provide[Containers.file_path_service],
+) -> None:
+    path = path_service.create_release_virtual_filesystem_path(
+        storage_scheme="az",
+        release=Config.BENCHMARK_DOPPA_DATA_RELEASE,
+        container=StorageContainer.DATA,
+        theme=Theme.BUILDINGS,
+        region="*",
+        file_name="*.parquet",
+    )
+
+    min_lon, min_lat, max_lon, max_lat = BoundingBox.TRONDHEIM_WGS84.value
+
+    db_context.execute(
+        f"""
+        SELECT * FROM read_parquet('{path}')
+        WHERE ST_Intersects(
+            geometry,
+            ST_MakeEnvelope(?, ?, ?, ?)
+        );
+        """,
+        [min_lon, min_lat, max_lon, max_lat],
+    )
